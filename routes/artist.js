aca40ecf256b84872bfb556da17b6a8192f86b87
David Rodriguez <davidgr80@gmail.com>
2017-10-25 16:09:59 -0300

    Update Artist2

---
+++ routes/artist.js
@@ -0,0 +1,16 @@
+/**
+ * Created by drodriguez on 24/10/17.
+ */
+'use strict'
+
+var express = require('express');
+var ArtistController = require('../controllers/artist');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+
+api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
+api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
+api.put('/artists/:id', md_auth.ensureAuth, ArtistController.updateArtist);
+
+module.exports = api;
