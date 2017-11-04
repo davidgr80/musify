24f6b3c1f38fc5ea006945c6cd9e125746072035
David Rodriguez <davidgr80@gmail.com>
2017-11-04 18:29:29 -0300

    Listo controlador artistas

--- controllers/artist.js
+++ controllers/artist.js
@@ -88,9 +88,88 @@ function saveArtist(req,res){
    })
 }
 
+function deleteArtist(req,res){
+    var artistId = req.params.id;
+
+    Artist.findByIdAndRemove(artistId, (err,artistRemoved)=>{
+        if(err){
+            res.status(500).send({message: 'Error al borrar el artista'});
+        }else{
+            if(!artistRemoved){
+                res.status(404).send({message: 'El artista no ha sido eliminado'});
+            }else{
+                res.status(202).send({artist: artistRemoved});
+                Album.find({artist: artistRemoved._id}).remove((err,albumRemove)=>{
+                    if(err){
+                        res.status(500).send({message: 'Error al borrar el album'});
+                    }else {
+                        if (!albumRemove) {
+                            res.status(404).send({message: 'El album no ha sido eliminado'});
+                        } else {
+                            Song.find({album: albumRemove._id}).remove((err,songRemove)=>{
+                                if(err){
+                                    res.status(500).send({message: 'Error al borrar la cancion'});
+                                }else {
+                                    if (!songRemove) {
+                                        res.status(404).send({message: 'La cancion no ha sido eliminada'});
+                                    } else {
+                                        res.status(200).send({artist: artistRemoved});
+                                    }
+                                }
+                            });
+
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
+
+function uploadImage(req,res){
+    var artistId = req.params.id;
+    var file_name = 'No subido...';
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('/');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('.');
+        var file_ext = ext_split[1];
+        if(file_ext == 'png' || file_ext == 'jpg'){
+            Artist.findByIdAndUpdate(artistId, {image: file_name}, (err, artistUpdated) => {
+                if(!userUpdated){
+                    res.status(404).send({message: 'No se ha podido actualizar el artista'});
+                } else {
+                    res.status(200).send({artist: artistUpdated});
+                }
+            });
+        } else {
+            res.status(200).send({message: 'Extension del archivo no valida'});
+        }
+    } else {
+        res.status(500).send({message: 'No has subido ninguna imagen.'});
+    }
+}
+
+function getImageFile(req,res){
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/artists/'+imageFile;
+    fs.exists(path_file, (exists) => {
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(404).send({message: 'No existe la imagen'});
+        }
+    });
+}
+
 module.exports = {
     getArtist,
     saveArtist,
     getArtists,
-    updateArtist
+    updateArtist,
+    deleteArtist,
+    uploadImage,
+    getImageFile
 };
